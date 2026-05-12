ed505c96ab341da94b319582e63417ab155f5352
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-13 00:53:34 +0500

    middleGeo

--- SCAD/src/App.jsx
+++ SCAD/src/App.jsx
@@ -1,12 +1,14 @@
 import Top from './components/top';
 import TopRec from './components/topRec';
 import Middle from './components/middle';
+import MiddleBottom from './components/middleBottom';
 function App() {
   return (  
 <div>
 <Top/>
 <TopRec/>
 <Middle/>
+<MiddleBottom/>
 </div>
 
   );
--- SCAD/src/components/middleBottom.jsx
+++ SCAD/src/components/middleBottom.jsx
@@ -1,9 +1,12 @@
 import imgGeo from './img/_14C7741_cr 1.png'
 
-const middleBottom = () => {
+const MiddleBottom = () => {
   return (
-    <div>middleBottom</div>
+    <div>
+       <div className=' mt-40 relative'><img src={imgGeo} alt="" className='brightness-50 ' /> <div className=' absolute top-5 left-70'><h1 className=' text-white font-extrabold text-5xl'>География проектов</h1> <p className='text-white text-2xl w-135 pt-8 font-medium'>Группа компаний SCAD tech имеет более 17 лет опыта в автомобильной промышленности и промышленной автоматизации.</p> <p className='w-135 text-gray-300 text-xl pt-8'>Мы занимаем прочную позицию на нефтяном и газовом рынке и является многолетним надежным партнером как для российских, так и для зарубежных компаний.</p><button className='mt-8 w-50 h-13 bg-white rounded-xl'>Подать заявку</button></div></div>
+
+    </div>
   )
 }
 
-export default middleBottom
+export default MiddleBottom
