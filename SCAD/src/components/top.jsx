725c318e83f96c1c18da3f6454d671adf965895b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-12 22:31:06 +0500

    jsx

--- SCAD/src/components/top.jsx
+++ SCAD/src/components/top.jsx
@@ -1,11 +1,13 @@
-import im from './img/Gr'
+import scad from './img/лого.png'
+import phone from './img/Group.png'
+import mail from './img/mail 1.png'
 
 
 const Top = () => {
   return (
     <div>
         <div>
-            <div><img src="" alt="" /></div><div><div></div></div>
+            <div><img src={scad} alt="" /><p>Комплексные решения в сфере управления технологиями</p></div><div><div><div className=''><img src={phone} alt="" /><p>+992 917 20 8222</p></div><div><img src={mail} alt="" /><p>mustafo.karimzoda@zet-mobile.com</p></div></div></div>
         </div>
 
     </div>
