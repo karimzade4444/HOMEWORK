1a94d2d2f087791d5edde97cb008979421d48804
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-12 21:58:00 +0500

    img

---
+++ SCAD/src/components/top.jsx
@@ -0,0 +1,15 @@
+import im from './img/Gr'
+
+
+const Top = () => {
+  return (
+    <div>
+        <div>
+            <div><img src="" alt="" /></div><div><div></div></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Top
