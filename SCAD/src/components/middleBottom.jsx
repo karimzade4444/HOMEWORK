8ff37f1355a2b8b3fdecbb235804729806f068d4
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-13 18:43:19 +0500

    middleBottom

--- SCAD/src/components/middleBottom.jsx
+++ SCAD/src/components/middleBottom.jsx
@@ -4,9 +4,15 @@ const MiddleBottom = () => {
   return (
     <div>
        <div className=' mt-40 relative'><img src={imgGeo} alt="" className='brightness-50 ' /> <div className=' absolute top-5 left-70'><h1 className=' text-white font-extrabold text-5xl'>География проектов</h1> <p className='text-white text-2xl w-135 pt-8 font-medium'>Группа компаний SCAD tech имеет более 17 лет опыта в автомобильной промышленности и промышленной автоматизации.</p> <p className='w-135 text-gray-300 text-xl pt-8'>Мы занимаем прочную позицию на нефтяном и газовом рынке и является многолетним надежным партнером как для российских, так и для зарубежных компаний.</p><button className='mt-8 w-50 h-13 bg-white rounded-xl'>Подать заявку</button></div></div>
-<div className='flex'>
+<div className='flex justify-center items-center gap-40 mt-20'>
     <div><h1 className=' font-bold text-5xl w-80'>Компания SCAD tech</h1><div className='w-30 h-1 bg-red-500 mt-10'></div><p className='mt-10 w-100'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente reprehenderit molestiae repellendus doloremque, asperiores, quo eaque laboriosam accusamus harum ratione, neque obcaecati. Velit voluptates laboriosam nulla corrupti atque in at!</p></div>
-    <div><div><p>17</p><p>лет опыта работы в промышленной автоматизации</p></div></div>
+    <div className=' grid grid-cols-2 gap-20'>
+      <div className=' flex flex-col items-center justify-center'><p className=' text-red-500 font-bold text-6xl'>17</p><p className=' w-42 text-xl '>лет опыта работы в промышленной автоматизации</p></div>
+      <div className=' flex flex-col items-center justify-center'><p  className=' text-red-500 font-bold text-6xl'>9000</p><p className=' w-42 text-xl '>кв.м - производственная площадь</p></div>
+      <div className=' flex flex-col items-center justify-center'><p className=' text-red-500 font-bold text-6xl'>11</p><p className=' w-42 text-xl '>офисов компании в городах России</p></div>
+      <div className=' flex flex-col items-center justify-center'><p className=' text-red-500 font-bold text-6xl'>920</p><p className=' w-42 text-xl '>квалифицированных сотрудников</p></div>
+    
+    </div>
 </div>
 
 
